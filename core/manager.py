4b1a7aebb750cd0065c4ef3debb92fa06c3fd600
semoleg1986 <semoleg1986@gmail.com>
2025-10-27 23:39:21 +0400

    docs: add sphinx-style docstrings to manager.py (Mon, Oct 27, 2025 11:39:21 PM)

--- core/manager.py
+++ core/manager.py
@@ -8,11 +8,29 @@ from utils.storage import load_data, save_data
 
 
 class PasswordManager:
+    """
+    Менеджер паролей для добавления, поиска и удаления учетных данных.
+
+    :param storage_path: Путь к файлу хранения паролей, по умолчанию PASSWORDS_FILE
+    :type storage_path: Path
+    """
+
     def __init__(self, storage_path: Path = PASSWORDS_FILE):
         self.storage_path = storage_path
         self.data = load_data(storage_path)
 
     def add_password(self, site: str, username: str, password: str) -> None:
+        """
+        Добавляет новый пароль для сайта.
+
+        :param site: Название сайта
+        :type site: str
+        :param username: Имя пользователя
+        :type username: str
+        :param password: Пароль пользователя
+        :type password: str
+        :raises EmptyInputError: Если любое из полей пустое
+        """
         if not all([site, username, password]):
             raise EmptyInputError("Все поля должны быть заполнены")
 
@@ -20,6 +38,15 @@ class PasswordManager:
         save_data(self.storage_path, self.data)
 
     def find_password(self, site: str) -> Tuple[str, str, str]:
+        """
+        Находит пароль для заданного сайта.
+
+        :param site: Название сайта
+        :type site: str
+        :return: Кортеж из (site, username, password)
+        :rtype: Tuple[str, str, str]
+        :raises PasswordNotFoundError: Если сайт не найден в базе
+        """
         try:
             record = self.data[site]
         except KeyError:
@@ -27,6 +54,13 @@ class PasswordManager:
         return site, record["username"], record["password"]
 
     def remove_password(self, site: str) -> None:
+        """
+        Удаляет пароль для заданного сайта.
+
+        :param site: Название сайта
+        :type site: str
+        :raises PasswordNotFoundError: Если сайт не найден в базе
+        """
         try:
             self.data.pop(site)
         except KeyError:
