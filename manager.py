c3b4c4c6bc29642317e0d7f1e83b33d5534ef469
semoleg1986 <semoleg1986@gmail.com>
2025-10-27 21:58:46 +0400

    refactor: impl. storage.py move save_data, load_data (Mon, Oct 27, 2025 09:58:46 PM)

--- manager.py
+++ manager.py
@@ -1,9 +1,8 @@
 import hashlib
-import json
 from pathlib import Path
-from typing import Any, Dict
 
 from constants import PASSWORDS_FILE
+from utils.storage import load_data, save_data
 
 
 class EmptyInputError(Exception):
@@ -19,35 +18,6 @@ def hash_password(password: str) -> str:
     :rtype: str
     """
     return hashlib.sha256(password.encode("utf-8")).hexdigest()
-
-
-def save_data(path: Path, data: Dict[str, Any]) -> None:
-    """
-    Сохраняет данные в JSON-файл.
-    """
-    json_text = json.dumps(data, indent=4, ensure_ascii=False)
-    path.write_text(json_text, encoding="utf-8")
-
-
-def load_data(path: Path) -> Dict[str, Any]:
-    """
-    Загружает данные из JSON-файла.
-    """
-    if not path.exists():
-        try:
-            path.parent.mkdir(parents=True, exist_ok=True)
-            path.write_text("{}", encoding="utf-8")
-        except OSError as e:
-            raise OSError(f"Ошибка создания файла '{path}': {e}") from e
-
-        try:
-            return json.loads(path.read_text(encoding="utf-8"))
-        except json.JSONDecodeError as e:
-            raise json.JSONDecodeError(
-                f"Ошибка разбора JSON в файле '{path}': {e.msg}", e.doc, e.pos
-            )
-        except OSError as e:
-            raise OSError(f"Ошибка чтения файла '{path}': {e}") from e
 
 
 class PasswordManager:
